9b478bbb8516e4d0b5785b60084bf8d155a682b5
Sakhawat Hossain Mridul <63304179+SH-Mridul@users.noreply.github.com>
2022-11-23 02:17:30 +0600

    comment added for making more informative

--- Main.java
+++ Main.java
@@ -10,38 +10,45 @@ public class Main {
 	}
 	
 	
-	
+	//spliting the array element
 	public static void split(int nums[],int left,int right)
-	{
+	{	//base case
 		if(left>=right)
 			return;
-		
+		//taking mid
 		int mid = left+(right-left)/2;
 		
+		//call for left elements
 		split(nums,left,mid);
+		
+		//call for right elements
 		split(nums,mid+1,right);
 		
+		//marge elements
 		marge(nums,left,mid,right);
 	}
 	
-	
+	//marge the spliting element
 	public static void marge(int nums[],int left,int mid,int right)
-	{
-		int n1 = mid-left+1;
-		int n2 = right - mid;
+	{	
 		
-		int leftElement[] = new int[n1];
-		int rightElement[]  = new int[n2];
+		int n1 = mid-left+1; //left elements size
+		int n2 = right - mid; // right element size
 		
+		int leftElement[] = new int[n1]; //for left element of array
+		int rightElement[]  = new int[n2]; // for right element of array
 		
+		//input left element
 		for(int i = 0; i<n1; i++)
 			leftElement[i] = nums[left+i];
 		
+		//input right element
 		for(int i = 0; i<n2; i++)
 			rightElement[i] = nums[mid+1+i];
-		
+		//index field
 		int i = 0, j = 0,k = left;
 		
+		//compare left and right then marge 
 		while(i<n1 && j<n2)
 		{
 			if(leftElement[i]<=rightElement[j])
